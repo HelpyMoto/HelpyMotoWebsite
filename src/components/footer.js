d1f42a6220d4d8538dc2221a0082a161ffcbbe67
helpymoto <officialbusiness.sb@gmail.com>
2023-04-05 19:08:30 +0530

    <Further Update>

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,184 +1,152 @@
-
-
 import React from 'react'
 import "../styles/footer.css"
 
 const footer = () => {
   return (
 
-  
-<>
-  {/* Remove the container if you want to extend the Footer to full width. */}
-  <div className="containerfooter my-5">
-    {/* Footer */}
-    <footer
-      className="text-center text-lg-start text-dark"
-      style={{ backgroundColor: "white" }}
-    >
-      {/* Section: Social media */}
-      <section
-        className="d-flex justify-content-between p-4 text-white"
-        style={{ backgroundColor: "black" }}
-      >
-        {/* Left */}
-        <div className="me-5">
-          <span>Get connected with us on social networks:</span>
-        </div>
-        
-        <div>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-facebook-f" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-twitter" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-google" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-instagram" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-linkedin" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-github" />
-          </a>
-        </div>
-        {/* Right */}
-      </section>
-      {/* Section: Social media */}
-      {/* Section: Links  */}
-      <section className="">
-        <div className="container text-center text-md-start mt-5">
-          {/* Grid row */}
-          <div className="row mt-3">
-            {/* Grid column */}
-            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              {/* Content */}
-              <img src={require("../assets/Logo.png")} alt="logo" className="img-fluidfooter" style={{ width:"100px",marginTop:"-40px" }} />
-             
-              <p>
-                    At our venture, we strive to restore motors all across India in an efficient manner that saves our customers' valuable time by eliminating the need to bring their cars to a repair shop
-
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 underlinefp">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Useful Links</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <a href="#!" className="text-dark">
-                  Home 
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  About Us
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Services
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark ">
-                Terms of service
-                </a>
-                  </p>
-                   <p>
-                <a href="#!" className="text-dark">
-                Privacy policy
-                </a>
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Practice Areas</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <a href="#!" className="text-dark">
-                Repairing
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                Engine
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Painting
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                Body and Tires
-                </a>
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Contact</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <i className="fas fa-home mr-2" />  Indranagar phase-2, New <span className="sidefooterbar" > Delhi-110001, India </span>
-              </p>
-              <p>
-                <i className="fas fa-envelope mr-2" /> officialbusiness.sb@gmail.com
-              </p>
-                  <p>
-                <i className="fas fa-phone mr-2" /> +91 9098630635
-              </p>
+    <footer id="footer">
+    {" "}
+    <div className="footer-newsletter">
+      {" "}
+      <div className="container1212">
+        {" "}
+        <div className="row">
+          {" "}
+          <div className="col-lg-6">
+            {" "}
+            <h4> Sign up to Our Newsletter</h4>{" "}
+            <p>
+            We will be consistently sending you valuable newsletters with substantial content.
+            </p>{" "}
+          </div>{" "}
+          <div className="col-lg-6">
+            {" "}
+            <form action="" method="post">
+              {" "}
+              <input class="a" type="email" name="email" placeholder="Email-Address" />
+              <input type="submit" defaultValue="Subscribe" />{" "}
+            </form>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>{" "}
+    <div className="footer-top">
+      {" "}
+      <div className="container1212">
+        {" "}
+        <div className="row">
+          {" "}
+          <div className="col-lg-3 col-md-6 footer-links abc">
+            {" "}
+            <h4>Useful Links</h4>{" "}
+            <ul>
+              {" "}
+              <li>
+                <i className="bx bx-chevron-right" /> <a href="/">Home</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" /> <a href="/">About us</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" /> <a href="/">Services</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" />{" "}
+                <a href="/">Terms of service</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" />{" "}
+                <a href="/">Privacy policy</a>
+              </li>{" "}
+            </ul>{" "}
+          </div>{" "}
+          <div className="col-lg-3 col-md-6 footer-links abc">
+            {" "}
+            <h4>Practice Areas</h4>{" "}
+            <ul>
+              {" "}
+              <li>
+                <i className="bx bx-chevron-right" /> <a href="/">Repairing</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" />{" "}
+                <a href="/">Engine</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" />{" "}
+                <a href="/">Painting</a>
+              </li>{" "}
+              <li>
+                <i className="bx bx-chevron-right" /> <a href="/">Body and Tires</a>
+              </li>{" "}
               
-            </div>
-            {/* Grid column */}
-          </div>
-          {/* Grid row */}
-        </div>
-      </section>
-      {/* Section: Links  */}
-      {/* Copyright */}
-      <div
-        className="text-center p-3"
-        style={{ backgroundColor: "black" }}
-          >
-            <span className="text-white">
-            © 2021-2023 Copyright : 
-            </span>
-       
-        <a className="text-white" href="#">
-        All Rights Reserved
-        </a>
-      </div>
-      {/* Copyright */}
-    </footer>
-    {/* Footer */}
-  </div>
-  {/* End of .container */}
-</>
-
-    
+            </ul>{" "}
+          </div>{" "}
+          <div className="col-lg-3 col-md-6 footer-links abc">
+            {" "}
+            <h4 >Contact Us</h4>{" "}
+            <p>
+              {" "}
+              New Delhi,Indranagar phase-2,
+              <br /> India<br />
+              <br /> <strong>Phone:</strong>+91 90986 30635
+              <br /> <strong>Email:</strong> officialbusiness.sb@gmail.com
+                <br />{" "}
+                <br />{" "}
+                
+              {/* <a href="#" title="Image from freepnglogos.com"><img src="https://www.freepnglogos.com/uploads/app-store-logo-png/google-play-and-apple-app-store-logos-22.png" width="250" alt="google play and apple app store logos"  /></a> */}
+             
+            </p>{" "}
+          </div>{" "}
+          <div className="col-lg-3 col-md-6 footer-links abc">
+            {" "}
+            {/* <h2> <img src={require("../image/image1.jpg")} alt="logo" className="img-fluid" style={{ width:"150px",marginTop:"-40px",marginBottom:"3px" }} /></h2>{" "} */}
+            <p className='j'>
+            At our venture, we strive to restore motors all across India in an efficient manner that saves our customers' valuable time by eliminating the need to bring their cars to a repair shop
+              </p>{" "}
+              <br />{" "}
+              <br />{" "}
+            <div className="social-links">
+              {" "}
+              <a href="https://twitter.com/HelpyMoto?t=FmxcdcAnmVX6GO20FkKFzA&s=09" className="twitter">
+                <i className="bx bxl-twitter" />
+              </a>{" "}
+              <a href="https://www.facebook.com/profile.php?id=100069828099402&mibextid=ZbWKwL" className="facebook">
+                <i className="bx bxl-facebook" />
+              </a>{" "}
+              <a href="https://instagram.com/helpy_moto?igshid=YmMyMTA2M2Y=" className="instagra">
+                <i className="bx bxl-instagram" />
+              </a>{" "}
+              <a href="https://www.linkedin.com/company/helpy-moto/mycompany/" className="linkedin">
+                <i className="bx bxl-linkedin" />
+                </a>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>{" "}
+    <br/><br/><br/>
+    <div className="container12">
+      {" "}<br/><br/>
+      <div className="copyright">
+        {" "}
+        © 2023 Helpy Moto{" "}
+        <strong>
+            <span></span>
+            <br />
+        </strong>
+         All Rights Reserved{" "}
+      </div>{" "}
+      <div className="credits">
+        {" "}
+        Designed by <a href="/">Helpy Moto</a>{" "}
+      </div>{" "}
+    </div>
+  </footer>
    
 
 
   )
 }
 
-export default footer;
+export default footer;e
